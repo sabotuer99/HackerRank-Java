660c317d68dee34b9a9d6a43e51d2bd15abda22d
Troy Whorten <sabotuer99@gmail.com>
2017-11-29 09:53:33 -0700

    Create KittyAndKatty.java

---
+++ HackerRank/src/algorithms/gametheory/KittyAndKatty.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        for(int a0 = 0; a0 < T; a0++){
+            int n = in.nextInt();
+            System.out.println(n==1 || n%2==0 ? "Kitty" : "Katty");
+        }
+    }
+}
